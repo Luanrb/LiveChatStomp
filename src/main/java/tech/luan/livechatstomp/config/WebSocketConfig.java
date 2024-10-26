fe63ff22c5887ff1162db353eea294596644bd6a
Luan Reis Becker <93626842+Luanrb@users.noreply.github.com>
2024-10-26 12:58:51 -0300

    Update WebSocketConfig.java

--- src/main/java/tech/luan/livechatstomp/config/WebSocketConfig.java
+++ src/main/java/tech/luan/livechatstomp/config/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/luan-livechatstomp-websocket");
+        registry.addEndpoint("/luan-livechatstomp-websocket")
+             .setAllowedOrigins("*");
     }
 }
